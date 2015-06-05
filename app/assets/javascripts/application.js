f54ccb6d020f838b8cb2f1771800c0d5fa14fb4d
Andrés López <andreslopezferro@gmail.com>
2015-06-04 19:23:59 -0500

    javascript para formulario.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,14 @@
 
 $(document).on("ready", function() {
 	// Codigo a ejecutar cuando el html esté listo.
+	$('form').submit(function(event) {
+		event.preventDefault();
+		var nombre = $('input#input_name').val();
+		var email = $('input#input_email').val();
+		var password = $('input#input_password').val();
+		$(".index span.name, .index span.name_dos").append(nombre);
+		$(".index span.email, .index span.email_dos").append(email);
+		$(".index span.password, .index span.password_dos").append(password);
+		$(this)[0].reset();
+	});
 });
