da79fc1941d85fd1673fac78289f10f88605b84d
Andrés López <andreslopezferro@gmail.com>
2015-06-05 18:17:20 -0500

    Mails base.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
-	default from: 'andreslopezferro@gmail.com'
-	def email_verification (username, email)
+	default from: 'correo@myapp.com'
+
+	def email_verification(username, email)
 		@name = username
 		@url = "http://www.google.com"
 		mail(to: email, subject: "Bienvenido.")
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => "app37553976@heroku.com",
+  :password       => "win2jfdj9034",
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
